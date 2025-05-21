33b6e0ac4eb2a7ef93cfc5d10b0aaf167188066e
Song Kim <song.kim@techbodia.com>
2025-05-21 17:11:34 +0700

    Delete nuxt-app directory

--- nuxt-app/nuxt.config.ts
+++
@@ -1,8 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  alias: {
-    '@core': '../core',
-  },
-  devtools: { enabled: true }
-})
