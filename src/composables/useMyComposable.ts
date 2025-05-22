bd45bd8b2105e68540b7f79c189c951de5aa8ba6
Song Kim <song.kim@techbodia.com>
2025-05-22 11:24:21 +0700

    feat: add goTo function to the composable to test router push

--- src/composables/useMyComposable.ts
+++ src/composables/useMyComposable.ts
@@ -1,7 +1,14 @@
 // src/composables/useMyComposable.ts
 import { ref } from 'vue';
+import { useRouter } from 'nuxt/app';
 
 export function useMyComposable() {
+  const router = useRouter();
   const message = ref("This is a composable from ps-core");
-  return { message };
+
+  const goTo = (path: string) => {
+    if (!path.length) return;
+    router.push(path);
+  }
+  return { message, goTo };
 }
